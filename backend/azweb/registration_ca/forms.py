f45bd52e037386c8440edd7e6f6a94a663a1f858
deepaktho <deepakthorat900@gmail.com>
2021-07-28 00:24:15 +0530

    .

--- backend/azweb/registration_ca/forms.py
+++ backend/azweb/registration_ca/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Extendeduser
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}), )
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
--- backend/azweb/registration_ca/models.py
+++ backend/azweb/registration_ca/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 # Create your models here.
 
 class Extendeduser(models.Model):
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.IntegerField()
@@ -12,6 +13,6 @@ class Extendeduser(models.Model):
     permanent_address = models.TextField()
     state  = models.CharField(max_length=100)
     email = models.EmailField()
-    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    # user = models.OneToOneField(User, on_delete= models.CASCADE)
 
 
