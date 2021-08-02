55bac70e0eec0a015e9e82f327147fd1de031cdc
deepaktho <deepakthorat900@gmail.com>
2021-08-03 01:57:28 +0530

    .

--- backend/azweb/azweb/urls.py
+++ backend/azweb/azweb/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('',include('registration_ca.urls')),
     path('admin/', admin.site.urls),
      
-    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+     
     path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
