88d2fb0f2c64458c138d573b660408400fddba61
deepaktho <deepakthorat900@gmail.com>
2021-08-03 02:07:42 +0530

    Update settings.py

--- backend/azweb/azweb/settings.py
+++ backend/azweb/azweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#&juysx(b8kh&$6+#&^*z+29s_ih7j5u$yt%&4gtclf1j0i%x^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
