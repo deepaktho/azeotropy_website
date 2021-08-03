eaf393c1081193f262f1093ff983ed5ba01cc1ce
deepaktho <deepakthorat900@gmail.com>
2021-08-03 23:33:32 +0530

    .

--- backend/azweb/registration_ca/urls.py
+++ backend/azweb/registration_ca/urls.py
@@ -7,7 +7,7 @@ app_name = "registration_ca"
 
 
 urlpatterns = [
-    path('/ca',views.Registration,name='index'),
+    path('ca',views.Registration,name='index'),
     path('register', views.user_register, name = "register"),
    
 ]
